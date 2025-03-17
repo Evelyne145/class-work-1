33eb6b759b4ab2b4c14ca28026c916780bd855e8
umuhirwa14 <umuhirwaevelyne@gmail.com>
2025-03-17 20:06:47 +0400

    third version

--- ppyfun.py
+++ ppyfun.py
@@ -1,4 +1,11 @@
-print("hello python")
-text= input("enter your text:")
-character_count=len(text.replace(" ", ""))
-print(f"character_count(excluding spaces)={character_count }")
+#print("hello python")
+#text= input("enter your text:")
+#character_count=len(text.replace(" ", ""))
+#print(f"character_count(excluding spaces)={character_count }")
+words=[]
+for i in range(1,6):
+    word = input(f"Enter word {i}:")
+    words.append(word)
+print("\nWord Lengths:")
+for word in words:
+    print(f"'{word}' has {len(word)}characters.")
